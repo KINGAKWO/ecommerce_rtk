766391e5651d7abe1d619bb14a38ce334d7fc0a8
KINGAKWO <kingakwomakembe@gmail.com>
2025-04-09 15:22:28 +0100

    added the components , created the store.js file

---
+++ src/Components/SuperCoin.jsx
@@ -0,0 +1,5 @@
+const [superCoins, setSuperCoins] = useState(0);
+const cartItems = useSelector(state => state.cart.cartItems);
+const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+
